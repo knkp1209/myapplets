e919b951f34e4ea9dce5133c71d906bcb4e0c9da
knkp1209 <30972393+knkp1209@users.noreply.github.com>
2017-08-23 15:21:13 +0800

    v0.0
    v0.0

---
+++ pages/index/index.js
@@ -0,0 +1,61 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 1200,
+    iconArray: [
+      {
+        "iconUrl": '../../image/user.png',
+        "iconText": '分类'
+      },
+      {
+        "iconUrl": '../../image/user.png',
+        "iconText": '分类'
+      },
+      {
+        "iconUrl": '../../image/user.png',
+        "iconText": '分类'
+      },
+      {
+        "iconUrl": '../../image/user.png',
+        "iconText": '分类'
+      }
+    ],
+    textArray:[
+      {
+        "bText":'标题'
+      }
+    ]
+  },
+  
+  changeIndicatorDots: function (e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function (e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function (e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  }
+})
