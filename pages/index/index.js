03f5d1c0364426eba7c19fde24c2b633c2eb21b2
knkp1209 <30972393+knkp1209@users.noreply.github.com>
2017-08-24 14:02:27 +0800

    v1.1
    v1.1

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,7 @@ Page({
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
     ],
+    swpimg:null,
     indicatorDots: true,
     vertical: false,
     autoplay: true,
@@ -33,29 +34,46 @@ Page({
     ],
     textArray:[
       {
-        "bText":'标题'
+        "bText":'标题',
+        "bText1":'标题1'
       }
     ]
   },
   
-  changeIndicatorDots: function (e) {
-    this.setData({
-      indicatorDots: !this.data.indicatorDots
-    })
-  },
-  changeAutoplay: function (e) {
-    this.setData({
-      autoplay: !this.data.autoplay
-    })
-  },
-  intervalChange: function (e) {
-    this.setData({
-      interval: e.detail.value
-    })
-  },
-  durationChange: function (e) {
-    this.setData({
-      duration: e.detail.value
+  // changeIndicatorDots: function (e) {
+  //   this.setData({
+  //     indicatorDots: !this.data.indicatorDots
+  //   })
+  // },
+  // changeAutoplay: function (e) {
+  //   this.setData({
+  //     autoplay: !this.data.autoplay
+  //   })
+  // },
+  // intervalChange: function (e) {
+  //   this.setData({
+  //     interval: e.detail.value
+  //   })
+  // },
+  // durationChange: function (e) {
+  //   this.setData({
+  //     duration: e.detail.value
+  //   })
+  // }
+  onLoad: function () {
+    var that = this//不要漏了这句，很重要
+    wx.request({
+      url: 'http://localhost/getswpimg.php?rid=1',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        //将获取到的json数据，存在名字叫zhihu的这个数组中
+        that.setData({
+          swpimg: res.data.result,
+          //res代表success函数的事件对，data是固定的，stories是是上面json数据中stories
+        })
+      }
     })
   }
 })
